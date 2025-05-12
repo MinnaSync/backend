e9314fcf0feb96604332eea5b3a4e54f6b33d645
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-05-12 15:36:54 -0400

    feat: requests proxy

---
+++ api/rest/proxy.go
@@ -0,0 +1,40 @@
+package rest
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Proxy(c *gin.Context) {
+	rawProxiedUrl := c.Param("url")[1:]
+	proxiedUrl, err := url.Parse(rawProxiedUrl)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	req, _ := http.NewRequest("GET", proxiedUrl.String(), nil)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	defer resp.Body.Close()
+
+	statusCode := resp.StatusCode
+	contentType := resp.Header.Get("Content-Type")
+	if contentType == "" {
+		contentType = "application/octet-stream"
+	}
+
+	response, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	c.Data(statusCode, contentType, response)
+}
--- api/rest/rest.go
+++ api/rest/rest.go
@@ -3,4 +3,5 @@ package rest
 import "github.com/gin-gonic/gin"
 
 func Register(r *gin.Engine) {
+	r.GET("/proxied/*url", Proxy)
 }
