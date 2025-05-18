8fd5a3d0479c18edc549890b23163bfc19eaaeee
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-05-18 14:41:03 -0400

    style: move around structs

--- internal/websockets/structures.go
+++ internal/websockets/structures.go
@@ -12,3 +12,37 @@ type ClientJoinRoom struct {
 type ClientLeaveRoom struct {
 	Username string `json:"username"`
 }
+
+type ClientTimeUpdated struct {
+	Paused      bool    `json:"paused"`
+	CurrentTime float64 `json:"current_time"`
+}
+
+type ClientStateUpdated struct {
+	Paused      *bool    `json:"paused"`
+	CurrentTime *float64 `json:"current_time"`
+}
+
+type ClientSendMessage struct {
+	Message string `json:"message"`
+}
+
+type ClientReceiveMessage struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+type ClientQueueMedia struct {
+	Title string `json:"title"`
+	URL   string `json:"url"`
+}
+
+type RoomMediaQueued struct {
+	Title string `json:"title"`
+	URL   string `json:"url"`
+}
+
+type RoomData struct {
+	NowPlaying *NowPlayingMedia `json:"now_playing"`
+	Queue      []QueuedMedia    `json:"queue"`
+}
