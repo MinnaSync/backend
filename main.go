cf2a11eb51ce7da3a1d382885bc82759b068060f
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-05-22 09:30:47 -0400

    fix: cors this is a reoccuring thing huh..

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/MinnaSync/minna-sync-backend/api/ws"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	wsRoutes := gin.Default()
 	wsRoutes.SetTrustedProxies(nil)
+
+	wsRoutes.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"https://minna.gura.sa.com"},
+		AllowMethods: []string{"GET", "POST", "OPTIONS"},
+	}))
+
 	ws.Register(wsRoutes)
+
 	wsRoutes.Run(":3001")
 }
